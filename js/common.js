7143101aee0ce27899b05d87e3fdf8f43a7f6eef
Hoommus <vlad.from.ukraine2@gmail.com>
2017-03-17 14:30:26 +0200

    Completed... something

--- js/common.js
+++ js/common.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
-	var rotation = 0;
-	let translation = 0;
-	let up = true
-	var myVar = setInterval( function() {
-		//document.getElementById("rotate").style.transform = "rotate(" + rotation + "deg)";
-		/*if (translation == 100){
-			up = false;
-		} else if (translation == -100) {
-			up = true;
-		}
-		if(up) { translation += 1; } else {	translation -= 1;}*/
-		//$("#move").css({"transform" : "translateY(" + translation + "%)"})
-		$(".nim-dialog").css({"transform" : "rotate(" + rotation + "deg)"});
-		rotation = rotation + 4;
-		console.log("loop");
-	}, 1);
+	
 });
-
+/*
+var rotation = 0;
 var myVar = setInterval( function() {
 		let arr = document.getElementsByClassName("nim-dialog");
 		for(let i = 0; i < arr.length; i++) {
@@ -25,3 +11,4 @@ var myVar = setInterval( function() {
 		rotation = rotation + 4;
 		console.log("loop");
 	}, 1);
+*/
