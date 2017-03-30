61b487554c00fbbf639176a6790c650e957fccf8
Hoommus <vlad.from.ukraine2@gmail.com>
2017-03-30 13:41:23 +0300

    Mage elements more responsive and reworked teammates section

--- js/common.js
+++ js/common.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 	$("#pause").fadeOut(1);
 	$("#top-one").height($(window).height() - 120);
 	/* setTimeout(() => {
-		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini/i.test(navigator.userAgent) ) {
 		} else {
-			$(".parallax-mirror").append('<iframe id="vimeo_player" src="https://player.vimeo.com/video/45196609?loop=1&title=0&byline=0&portrait=0" width=100% height=100% frameborder="0"></iframe>');
+			$(".parallax-mirror").append('<iframe id="vimeo_player"
+			src="https://player.vimeo.com/video/45196609?loop=1&title=0&byline=0&portrait=0" 
+			width=100% height=100% frameborder="0"></iframe>');
 			console.log("logged!");
 			let iframe = document.querySelector('#vimeo_player'),
             player = new Vimeo.Player(iframe);
@@ -27,4 +29,9 @@ $(document).ready(function() {
 	}, 1000)
 	*/
 
+	$(".gallery-jq").hover(() => {
+		$(".gallery").animate({marginBottom: "-20px"}, 600);
+	}, () => {
+		$(".gallery").animate({marginBottom: "-100px"}, 600);
+	} );
 });
